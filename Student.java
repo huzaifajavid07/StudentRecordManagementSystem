f5e6c33ff5a1379cb5d9e941475c2e64bac1a4c8
Hafiz Muhammad Huzaifa Javid <168843439+huzaifajavid07@users.noreply.github.com>
2026-06-03 13:32:02 +0500

    Initial Student Program

---
+++ Student.java
@@ -0,0 +1,15 @@
+package StudentRecordManagementSystem;
+
+public class Student {
+
+    public static void main(String[] args) {
+
+        int studentId = 101;
+        String studentName = "Huzaifa Javid";
+        double cgpa = 3.5;
+
+        System.out.println("Student ID: " + studentId);
+        System.out.println("Student Name: " + studentName);
+        System.out.println("CGPA: " + cgpa);
+    }
+}
